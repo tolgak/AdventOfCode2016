47bdce84a64c2204376b30748d8b14ce9ca57c5c
Tolga Kurkcuoglu <tolga.kurkcuoglu@gmail.com>
2016-12-20 03:34:39 +0200

    concurrency example

--- src/puzzles/puzzle2.go
+++ src/puzzles/puzzle2.go
@@ -2,10 +2,28 @@ package puzzle2
 
 import (
 	"fmt"
+	"sync"
+	"time"
+)
+
+var (
+	counter = 0
+	lock    sync.Mutex
 )
 
 // Run ...
 func Run() {
+	for i := 0; i < 20; i++ {
+		go incr()
+	}
+	time.Sleep(time.Millisecond * 10)
 
 	fmt.Println("DONE...")
+}
+
+func incr() {
+	lock.Lock()
+	defer lock.Unlock()
+	counter++
+	fmt.Println(counter)
 }
